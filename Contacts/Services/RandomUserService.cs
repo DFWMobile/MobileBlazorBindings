d12e793b3d083607c82f9cb47e2bfc746609a8b3
Sean Sparkman <sean.sparkman@iinteractive.com>
2020-02-19 20:54:34 -0600

    Initial commit

---
+++ Contacts/Services/RandomUserService.cs
@@ -0,0 +1,24 @@
+﻿using Contacts.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.Services
+{
+    public class RandomUserService
+    {
+        public async Task<RandomUserData> GetUserData(int size)
+        {
+            var client = new HttpClient();
+
+            var response = await client.GetAsync($"https://randomuser.me/api/?results={size}&seed=dfwmobilenet");
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<RandomUserData>(json);
+        }
+    }
+}
